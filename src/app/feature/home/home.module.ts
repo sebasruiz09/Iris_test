6e9fb0cd2296a7974b7962de5f89825e331b483f
sebasruiz09 <sebasruiz909@gmail.com>
2022-12-29 19:41:25 -0500

    ADD : new favicon and toogle creation for theme

--- src/app/feature/home/home.module.ts
+++ src/app/feature/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { CoreModule } from '../../core/core.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ListComponent } from './components/list/list.component';
@@ -18,7 +17,6 @@ import { SharedModule } from '../../shared/shared.module';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    CoreModule,
     ReactiveFormsModule,
     SharedModule,
     FontAwesomeModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, CoreModule],
   exports: [FooterComponent, HeaderComponent],
 })
 export class SharedModule {}
