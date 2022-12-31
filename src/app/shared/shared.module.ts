ad8c5f3e280d689dfe7ce6dbe09a6c8432e9c04f
sebasruiz09 <sebasruiz909@gmail.com>
2022-12-30 21:54:08 -0500

    FIX : footer delete and solve some errors in responsive design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [HeaderComponent],
   imports: [CommonModule, CoreModule],
-  exports: [FooterComponent, HeaderComponent],
+  exports: [HeaderComponent],
 })
 export class SharedModule {}
